ee8a0fea8bc4696369eaf439287ad4b502561bab
Derek Wheeler <frozenarrow73@gmail.com>
2023-04-12 16:15:32 -0600

    day 2 code along, added states

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
+import {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
 import Bravo from './components/Bravo';
 import Charlie from './components/Charlie'
 
 function App() {
+const [userInput, setUserInput] = useState("")
+const [name, setName] = useState("")
+const [complete, setComplete] = useState(false)
 
   let person = "Brady"
 
+  const handleChange = (event) => {
+    setUserInput (event.target.value)
+  }
+
+  const handleClick = () => {
+    setName(userInput)
+    setUserInput("")
+  }
+
   return (
     <div className="App">
-      <h1>app</h1>
+      <h1>app {name}</h1>
+      <input placeholder="What's your name?" value={userInput} onChange={handleChange}/>
+      <button onClick={handleClick}>Change Name</button>
       <Bravo personName={person}/>
       <Bravo personName="Dave"/>
       <Bravo personName="Katie"/>
